05c226021e5f6e57d85d49a7f642486395d673b8
Alexis <mfjcol@icloud.com>
2022-01-05 10:37:36 -0300

    By Renato Costa

--- exercio1_Metodos/src/Calculadora.java
+++ exercio1_Metodos/src/Calculadora.java
@@ -1,24 +1,24 @@
 public class Calculadora {
 	
-	public static void soma  (double numero1, double numero2 ) {
-		double resultado = numero1 + numero2;
-		System.out.println("A soma de " + numero1 + " e de " + numero2 + " e de " + resultado);
+	public static void soma  (double num1, double num2 ) {
+		double resultado = num1 + num2;
+		System.out.println("A soma de " + num1 + " e de " + num2 + " e de " + resultado);
 		
 	}
 	
-	public static void subtracao ( double numero1, double numero2) {
-		double resultado = numero1 - numero2;
-		System.out.println("A subtacao de " + numero1 + " e de " + numero2 + " e de " + resultado);
+	public static void subtracao ( double num1, double num2) {
+		double resultado = num1 - num2;
+		System.out.println("A subtacao de " + num1 + " e de " + num2 + " e de " + resultado);
 	}
 	
-	public static void divicao ( double numero1, double numero2) {
-		double resultado = numero1 / numero2;
-		System.out.println("A divicao de " + numero1 + " e de " + numero2 + " e de " + resultado);
+	public static void divicao ( double num1, double num2) {
+		double resultado = num1 / num2;
+		System.out.println("A divicao de " + num1 + " e de " + num2 + " e de " + resultado);
 		
 	}
 	
-	public static void multiplicacao ( double numero1, double numero2) {
-		double resultado = numero1 * numero2;
-		System.out.println("A Multiplicacao de " + numero1 + " e de " + numero2 + " e de "+ resultado);
+	public static void multiplicacao ( double num1, double num2) {
+		double resultado = num1 * num2;
+		System.out.println("A Multiplicacao de " + num1 + " e de " + num2 + " e de "+ resultado);
 	}
 }
